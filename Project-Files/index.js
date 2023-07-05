b466d23e74970c23eeacf37e714ac6a5b9e9fe77
+ <+>
2023-07-05 09:16:42 +0300

    Added event listener on like Button

---
+++ Project-Files/index.js
@@ -0,0 +1,5 @@
+// Event listeners for like, dislike, and refresh buttons
+document.getElementById("like-button").addEventListener("click", function() {
+    // Implement your like logic here
+    console.log("Liked!");
+});
