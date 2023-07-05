340dfa4eae1dae9ebb0315329150c86a48e06f71
+ <+>
2023-07-05 23:46:11 +0300

    Trying to fetch joke data

--- Project-Files/index.js
+++ Project-Files/index.js
@@ -1,3 +1,13 @@
+function fetchData() {
+  fetch('https://icanhazdadjoke.com/slack')
+    .then(response => {response.json()
+    return response.json();
+    })
+    .then(jokeData => {
+        console.log(data.jokeData)
+    }) 
+}
+
 // Event listeners for like, dislike, and refresh buttons
 document.getElementById("like-button").addEventListener("click", function() {
     console.log("Liked!");
