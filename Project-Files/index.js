22e7de041f00d90e9bbbcf2af17a5dea5819e8c9
+ <+>
2023-07-05 09:30:05 +0300

    event listener & Message for dislike

--- Project-Files/index.js
+++ Project-Files/index.js
@@ -1,5 +1,8 @@
 // Event listeners for like, dislike, and refresh buttons
 document.getElementById("like-button").addEventListener("click", function() {
-    // Implement your like logic here
     console.log("Liked!");
+});
+
+document.getElementById("dislike-button").addEventListener("click", function() {
+    console.log("Disliked - Not Funny!");
 });
