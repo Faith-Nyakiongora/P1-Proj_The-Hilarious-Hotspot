26f6889716c1febb5d70c5c64f4943ec05fe5054
+ <+>
2023-07-06 23:30:13 +0300

    Adding event listeners on the comment sections &Appending new comment section

--- index.js
+++ index.js
@@ -18,3 +18,25 @@ document.getElementById("like-button").addEventListener("click", function() {
 document.getElementById("dislike-button").addEventListener("click", function() {
     console.log("Disliked - Not Funny!");
 });
+
+const commentInput = document.getElementById('comment-input');
+const submitButton = document.getElementById('submit-button');
+
+// Get the comment section element
+const commentSection = document.getElementById('comment-section');
+
+// Event listener for the submit button
+submitButton.addEventListener('click', function() {
+  // Get the comment text from the input field
+  const commentText = commentInput.value;
+
+  // Create a new paragraph element to display the comment
+  const newComment = document.createElement('p');
+  newComment.textContent = commentText;
+
+  // Append the new comment to the comment section
+  commentSection.appendChild(newComment);
+
+  // Clear the input field
+  commentInput.value = '';
+})
